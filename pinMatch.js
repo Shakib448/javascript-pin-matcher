c608df542197db966041aad09cbbf71aaf5bbc7e
Shakib448 <shakiba448@gmail.com>
2020-07-28 11:07:48 +0600

    first commit

--- pinMatch.js
+++ pinMatch.js
@@ -1,4 +1,9 @@
 function generatePin() {
-    // 4 digit random number
-    document.getElementById("generatePin").value = (Math.round(1000 + Math.random() * 9000));
-  }
+  // 4 digit random number
+  document.getElementById("generatePin").value = (Math.round(1000 + Math.random() * 9000));
+}
+
+function removeNumbers() {
+  const number = document.getElementById("numberKeypad").value;
+  document.getElementById("numberKeypad").value = number.substring(0, number.length - 1)
+}
