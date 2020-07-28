f2ccd7245839683939d3d6d9ed84db8e94948630
Shakib448 <shakiba448@gmail.com>
2020-07-28 14:34:43 +0600

    first commit

--- pinMatch.js
+++ pinMatch.js
@@ -4,6 +4,22 @@ function generatePin() {
 }
 
 function removeNumbers() {
+  // One by one number remove function 
   const number = document.getElementById("numberKeypad").value;
   document.getElementById("numberKeypad").value = number.substring(0, number.length - 1)
+}
+
+function validPinCode() {
+  const generate =  document.getElementById('generatePin').value;
+  const numberPad = document.getElementById('numberKeypad').value;
+
+  const regex = /^[0-9]{4}$/;
+  
+  if(regex.test(generate) == regex.test(numberPad)){
+    alert("thanks for valid code");
+  }else if ((regex.test(generate) != regex.test(numberPad))){
+    alert("sorry the code is not matched")
+  }else{
+    return "something is went wrong";
+  }
 }
